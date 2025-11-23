782df8611fc2c45f80ab12c87b6a2ab81dfe09e1
runsenwu <84985987+runsenwu@users.noreply.github.com>
2025-11-23 08:29:00 -0800

    clean up

--- day1-part1/target/debug/day1.d
+++ day1-part1/target/debug/day1.d
@@ -1,1 +1,1 @@
-c:\Users\runse\Documents\advent_of_code_2024\day1\target\debug\day1.exe: c:\Users\runse\Documents\advent_of_code_2024\day1\src\main.rs
+C:\Users\runse\Documents\advent_of_code_2024\day1-part1\target\debug\day1.exe: C:\Users\runse\Documents\advent_of_code_2024\day1-part1\src\main.rs
